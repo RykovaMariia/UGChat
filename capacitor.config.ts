c4d2ea32c19af7254ad16d624907e3a851bc425e
Mariia Rykova <maryrykova24@gmail.com>
2025-06-07 23:17:07 +0400

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ugchat',
+  appName: 'ugchat',
+  webDir: 'dist'
+};
+
+export default config;
