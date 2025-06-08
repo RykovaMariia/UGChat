6782161f0b68dc682867a22ce62e6915bf4d5a5b
Mariia Rykova <maryrykova24@gmail.com>
2025-06-08 18:55:50 +0400

    chore: off vue/multi-word-component-names

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-deprecated-slot-attribute": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "vue/no-unused-components": "error",
-    "vue/no-undef-components": "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-unused-components': 'error',
+    'vue/no-undef-components': 'error',
+    'vue/multi-word-component-names': 'off',
   },
 };
